51aeacce3989d1b06be1c8b7fa095d41aebe0f5c
debrupa24 <56958356+debrupa24@users.noreply.github.com>
2026-03-27 20:43:55 +0530

    Update hosting.py

--- Predictive_Maintenance/hosting/hosting.py
+++ Predictive_Maintenance/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="Predictive_Maintenance/deployment",     # the local folder containing your files
-    repo_id="debrupa24/engine-maintenance-prediction",          # the target repo
+    repo_id="debrupa24/engine-maintenance-prediction-v1",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
